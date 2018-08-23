245ef19345de99b98f3fa4d82cb4b7ee8086d55b
Rohit Rai <rohitkrai03@gmail.com>
2018-08-24 02:20:34 +0530

    refactor(http-client): add tests in provider service

--- src/app/shared/account/provider.service.spec.ts
+++ src/app/shared/account/provider.service.spec.ts
@@ -2,7 +2,8 @@ import { TestBed } from '@angular/core/testing';
 
 import {
   HttpClientTestingModule,
-  HttpTestingController
+  HttpTestingController,
+  TestRequest
 } from '@angular/common/http/testing';
 
 import { Broadcaster, Logger } from 'ngx-base';
@@ -22,7 +23,10 @@ describe('Service: Provider Service', () => {
 
     beforeEach(() => {
       const mockAuthenticationService: jasmine.SpyObj<AuthenticationService> = createMock(AuthenticationService);
-      mockAuthenticationService.getToken.and.returnValue('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiY2xpZW50X3Nlc3Npb24iOiJURVNUU0VTU0lPTiIsInNlc3Npb25fc3RhdGUiOiJURVNUU0VTU0lPTlNUQVRFIiwiYWRtaW4iOnRydWUsImp0aSI6ImY5NWQyNmZlLWFkYzgtNDc0YS05MTk0LWRjM2E0YWFiYzUwMiIsImlhdCI6MTUxMDU3MTMxOSwiZXhwIjoxNTEwNTgwODI3fQ.l0m6EFvk5jbND3VOXL3gTkzTz0lYQtPtXS_6C24kPQk');
+
+      // We need to mock an actual jwt token here since this token is decoded by the servie to form legacry URL.
+      const mockToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiY2xpZW50X3Nlc3Npb24iOiJURVNUU0VTU0lPTiIsInNlc3Npb25fc3RhdGUiOiJURVNUU0VTU0lPTlNUQVRFIiwiYWRtaW4iOnRydWUsImp0aSI6ImY5NWQyNmZlLWFkYzgtNDc0YS05MTk0LWRjM2E0YWFiYzUwMiIsImlhdCI6MTUxMDU3MTMxOSwiZXhwIjoxNTEwNTgwODI3fQ.l0m6EFvk5jbND3VOXL3gTkzTz0lYQtPtXS_6C24kPQk';
+      mockAuthenticationService.getToken.and.returnValue(mockToken);
       mockLogger = jasmine.createSpyObj<Logger>('Logger', ['error']);
 
       TestBed.configureTestingModule({
@@ -30,7 +34,7 @@ describe('Service: Provider Service', () => {
         providers: [
           { provide: Logger, useValue: mockLogger },
           { provide: AuthenticationService, useValue: mockAuthenticationService },
-          { provide: AUTH_API_URL, useValue: 'https://auth.fabric8.io/api/' },
+          { provide: AUTH_API_URL, useValue: 'http://example.com/api/' },
           { provide: Broadcaster, useValue: BroadcasterTestProvider.broadcaster },
           ProviderService
         ]
@@ -39,8 +43,39 @@ describe('Service: Provider Service', () => {
       controller = TestBed.get(HttpTestingController);
     });
 
-    it('Get legacy linking url', () => {
+    it('should get legacy linking url', () => {
       let val = service.getLegacyLinkingUrl('openshift-v3', 'testredirect');
-      expect(val).toEqual('https://auth.fabric8.io/api/link/session?clientSession=TESTSESSION&sessionState=TESTSESSIONSTATE&redirect=testredirect&provider=openshift-v3');
+      expect(val).toEqual('http://example.com/api/link/session?clientSession=TESTSESSION&sessionState=TESTSESSIONSTATE&redirect=testredirect&provider=openshift-v3');
+    });
+
+    it('should check github connection status', (done: DoneFn) => {
+      const mockResponse = {
+        username: 'some-github-user'
+      };
+      service.getGitHubStatus().subscribe(resp => {
+        expect(resp).toBe(mockResponse);
+        controller.verify();
+        done();
+      });
+
+      const req: TestRequest = controller.expectOne('http://example.com/api/token?force_pull=true&for=https://github.com');
+      expect(req.request.method).toEqual('GET');
+      req.flush(mockResponse);
+    });
+
+    it('should check OSO connection status', (done: DoneFn) => {
+      const mockResponse = {
+        username: 'some-oso-user'
+      };
+      const cluster = 'some-cluster';
+      service.getOpenShiftStatus(cluster).subscribe(resp => {
+        expect(resp).toBe(mockResponse);
+        controller.verify();
+        done();
+      });
+
+      const req: TestRequest = controller.expectOne(`http://example.com/api/token?force_pull=true&for=${cluster}`);
+      expect(req.request.method).toEqual('GET');
+      req.flush(mockResponse);
     });
 });
